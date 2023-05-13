71c51eb96b735adca4af95aa34667a779ec48e38
pratish99 <124140641+pratish99@users.noreply.github.com>
2023-05-13 11:32:27 +0530

    Add files via upload

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <>
+            <h1 style={{ color: "orangedRed", fontSize: "3em", fontFamily: "cursive" }}>Pratish Soni</h1>
+            <h1 style={{ color: "orangedRed", fontSize: "3em", fontFamily: "cursive" }}>202212033</h1>
+        </>
+    )
+}
+
+export default About;
